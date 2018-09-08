a48b516bc55de58d302656ee78d0e81734af8288
YWJamesLin <ywjameslin@gmail.com>
2018-09-08 18:10:05 +0800

    [Facebook] Use environment variables to auto fetch asset

--- src/assets/FacebookAsset.js
+++ src/assets/FacebookAsset.js
@@ -2,6 +2,6 @@
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v3.1&appId=588552984839617&autoLogAppEvents=1';
+    js.src = 'https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v3.1&appId=' + process.env.VUE_APP_FACEBOOK_LOGIN_APP_ID + '&autoLogAppEvents=1';
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
