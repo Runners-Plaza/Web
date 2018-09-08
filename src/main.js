171bf349747c76e856bf65de7fed76eff546184d
YWJamesLin <ywjameslin@gmail.com>
2018-09-08 09:42:50 +0800

    [Web] Include vue-resource

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import '@babel/polyfill'
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import './plugins/vuetify'
 import './plugins/global'
 import App from './App.vue'
 import router from './router'
 
+Vue.use(VueResource)
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  http: {
+  }
 }).$mount('#app')
