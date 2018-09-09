40b888ad565d68180dbfeabfcee891c7fda4062f
YWJamesLin <ywjameslin@gmail.com>
2018-09-09 10:22:35 +0800

    [Facebook] Move Facebook to be a plugin

---
+++ src/plugins/facebook.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import '../assets/FacebookAsset.js'
+
+const Facebook = {
+  install(Vue) {
+    Vue.prototype.facebook = {
+      checkLoginState() {
+        let self = this;
+        FB.getLoginStatus(function (response) {
+          self.statusChangeCallback(response);
+        });
+      },
+      login() {
+        let self = this;
+        FB.login(
+          function (response) {
+            if (response.authResponse) {
+              self.statusChangeCallback(response);
+            }
+          },
+          {
+            scope: "email, public_profile",
+            return_scopes: true
+          }
+        );
+      },
+      statusChangeCallback(response) {
+        if (response.status === "connected") {
+          this.success(response);
+        } else {
+          this.login();
+        }
+      },
+      success: Function,
+      afterSuccess(success) {
+        this.success = success;
+      }
+    }
+  }
+}
+
+Vue.use(Facebook)
