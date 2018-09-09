0305a07d415f728fd6d969d68380357d7f709b33
YWJamesLin <ywjameslin@gmail.com>
2018-09-09 18:37:17 +0800

    [Plugin] Change asset loading path on facebook.js

--- src/plugins/facebook.js
+++ src/plugins/facebook.js
@@ -1,39 +1,39 @@
 import Vue from 'vue'
-import '../assets/FacebookAsset.js'
+import '@/assets/FacebookAsset.js'
 
 const Facebook = {
   install(Vue) {
     Vue.prototype.facebook = {
       checkLoginState() {
-        let self = this;
+        let self = this
         FB.getLoginStatus(function (response) {
-          self.statusChangeCallback(response);
-        });
+          self.statusChangeCallback(response)
+        })
       },
       login() {
-        let self = this;
+        let self = this
         FB.login(
           function (response) {
             if (response.authResponse) {
-              self.statusChangeCallback(response);
+              self.statusChangeCallback(response)
             }
           },
           {
             scope: "email, public_profile",
             return_scopes: true
           }
-        );
+        )
       },
       statusChangeCallback(response) {
         if (response.status === "connected") {
-          this.success(response);
+          this.success(response)
         } else {
-          this.login();
+          this.login()
         }
       },
       success: Function,
       afterSuccess(success) {
-        this.success = success;
+        this.success = success
       }
     }
   }
