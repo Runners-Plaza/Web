105d036c3148a7b28d35a2c1ac0fdf488262db80
YWJamesLin <ywjameslin@gmail.com>
2018-09-09 16:13:19 +0800

    [Web] Add toast component and show toast when logged in

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <the-toolbar />
     <v-content>
       <router-view />
+      <the-toast />
     </v-content>
     <the-footer />
   </v-app>
@@ -13,21 +14,23 @@
 import TheSidebar from './components/TheSidebar';
 import TheToolbar from './components/TheToolbar';
 import TheFooter from './components/TheFooter';
+import TheToast from './components/TheToast';
 
 export default {
   name: 'App',
   components: {
     TheSidebar,
     TheToolbar,
-    TheFooter
+    TheFooter,
+    TheToast,
   },
-  methods: {},
   created() {
     let self = this;
     this.facebook.afterSuccess((response) => {
       self.oauth.setTokenData(response.authResponse);
       self.runnersPlaza.setTokenData(response.authResponse);
       self.login().then(() => {
+        this.toaster.success('Logged in.');
         self.$router.replace({ name: 'home' });
       });
     });
@@ -37,7 +40,9 @@ export default {
         this.$router.replace({ name: 'home' });
       }
       this.runnersPlaza.setTokenData(tokenData);
-      this.login();
+      this.login().then(() => {
+        this.toaster.success('Logged in.');
+      });
     } else {
       this.$router.replace({ name: 'login' });
     }
