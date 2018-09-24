acf0b98f7728f4497b6d94b694e211643feb6e1a
YWJamesLin <ywjameslin@gmail.com>
2018-09-24 15:42:21 +0800

    [Router] Add member management route

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
+import Users from './views/Users.vue'
 
 Vue.use (Router)
 
@@ -10,12 +11,17 @@ export default new Router ({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
     },
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: Login,
+    },
+    {
+      path: '/users',
+      name: 'users',
+      component: Users,
     },
   ]
 })
