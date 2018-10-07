ccf79164a9c0d37240e50e3504a17bdc599427c6
YWJamesLin <ywjameslin@gmail.com>
2018-10-07 10:22:14 +0800

    [Profile] Remove redundant code

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,13 +2,13 @@
   <v-form v-model="valid" class="profile ma-3">
     <v-text-field
       v-model="name"
-      :label="this.$t ('name')"
+      :label="$t ('name')"
       required
     ></v-text-field>
     <v-text-field
       v-model="email"
       :rules="emailRules"
-      :label="this.$t ('email')"
+      :label="$t ('email')"
       required
     ></v-text-field>
     <v-btn
@@ -18,7 +18,7 @@
       color="teal darken-1"
       @click="updateProfile(name, email)"
     >
-    {{ this.$t ('save') }}
+    {{ $t ('save') }}
     </v-btn>
   </v-form>
 </template>
