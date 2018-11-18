4d7281478b7aa0292400ce8554515a517ad24f68
YWJamesLin <ywjameslin@gmail.com>
2018-11-18 15:43:18 +0800

    [Pagination] Add plugin and related component

---
+++ src/components/ThePagination.vue
@@ -0,0 +1,40 @@
+<template>
+  <div @update-paging="updatePaging" class="text-xs-center">
+    <v-pagination
+      v-model="currentPage"
+      :length="lastPage"
+      @input="$emit('change-page', currentPage)"
+    ></v-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "the-pagination",
+  props: [
+    'reloadPaging',
+  ],
+  data () {
+    return {
+      currentPage: 1,
+      lastPage: 1,
+    }
+  },
+  mounted () {
+    this.currentPage = 1
+  },
+  methods: {
+    updatePaging () {
+      let paging = this.pagination.getPaging ()
+
+      this.currentPage = paging.page
+      this.lastPage = parseInt(paging.last_page)
+    },
+  },
+  watch: {
+    reloadPaging () {
+      this.updatePaging ()
+    },
+  },
+}
+</script>
---
+++ src/plugins/pagination.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+
+const paging = {
+  page: 1,
+  per_page: 1,
+  last_page: 1,
+}
+
+const pagination = {
+  updateLastPage (lastPage) {
+    Object.assign (paging, {
+      last_page: lastPage,
+    })
+  },
+  updatePage (page) {
+    Object.assign (paging, {
+      page: page,
+    })
+  },
+  getPaging () {
+    return paging
+  },
+  appendPaging(url) {
+    return url + '?page=' + paging.page + '&per_page=' + paging.per_page
+  },
+  resetPaging () {
+    Object.assign (paging, {
+      page: 1,
+    })
+  }
+}
+
+const Pagination = {
+  install (Vue) {
+    Vue.mixin ({
+      data () {
+        return {
+          pagination
+        }
+      },
+    })
+  }
+}
+
+Vue.use (Pagination)
+
+export default pagination
