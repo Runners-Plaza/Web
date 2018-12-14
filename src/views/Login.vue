403d7334f6ed86ec6def55e22a34404582f6bd09
YWJamesLin <ywjameslin@gmail.com>
2018-12-15 07:59:21 +0800

    [Web] Remove vuetify icon on login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,6 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
         <FacebookLoginButton />
       </v-layout>
     </v-slide-y-transition>
