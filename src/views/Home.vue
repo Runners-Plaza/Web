2b43adf9235e539110de51d053edbadba246c268
YWJamesLin <ywjameslin@gmail.com>
2018-12-15 08:01:07 +0800

    [Home] Remove vuetify default home and fill in ours

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,7 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
+        <h6 class="title">{{ $t ('main_page_text') }}</h6>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
